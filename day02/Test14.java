d6cba40a1bf63cdde584cbea94cdea7ece50e724
chocchic <60125582+chocchic@users.noreply.github.com>
2022-02-08 15:47:06 +0900

    Update Test14.java

--- day02/Test14.java
+++ day02/Test14.java
@@ -17,9 +17,20 @@ public class Test14 {
 		// 논리 연산자 : && ||
 		// A && B : A와 B 둘다 참이여야 참
 		// A || B : A와 B 둘다 거짓이어야 거짓 -> 둘중 하나만 참이면 참
-		result = a > b && a<5;
+		result = a > b && a<5; // false (a<5가 false이므로)
+		System.out.println(result);
+		result = a< b || a<5; // false (a<b와 a<5둘다 false)
+		System.out.println(result);
+		result = a > 20 && a>b || a>5; // a>20 = F, a>b = T, a>5 = T (F&&T)||T = T
+		System.out.println(result);
+		result = a==20|| a>b&& a<5; //&&연산 먼저 F||(T&&F) = F
 		System.out.println(result);
 		
+		// 삼항연산자 : 조건식 ? 참일때 값 : 거짓일때 값;
+		int result2 = a > b ? a : b; // a가 크니까 a return 출력값 : 10
+		System.out.println(result2);
+		char result3 = a < b ? '0':'X';//a가 크므로 False 출력값 : 'X' 
+		System.out.println(result3);
+		
 	}
-
 }
