5c40095749ef9d68fae0ab4497dc07e6f1887353
chocchic <60125582+chocchic@users.noreply.github.com>
2022-02-14 10:41:23 +0900

    Add files via upload

--- teacher/day04/Test45.java
+++ teacher/day04/Test45.java
@@ -1,46 +1,97 @@
 package day04;
 
+import java.util.Scanner;
+
+/*	
+	문제1. Up & Down 게임 
+		0 ~ 99 사이의 임의의 수(랜던값)를 받아 숨기고, 그 수를 맞추는 게임. 
+		임의의 수보다 낮게 입력하면 "Up" 출력, 
+		높게 입력하면 "Down" 출력하면서 범위를 좁혀가며 수를 맞춘다. 
+		게임을 반복하기 위해 y/n으로 묻고, n인 경우 프로그램 종료 
+		
+	콘솔예) 
+	숫자가 정해졌습니다. 맞춰보세요! 
+	0~99
+	1 >>	55 (사용자가 입력) 
+	"Up"
+	55~99
+	2 >>	70
+	"Up"
+	70~99
+	3 >>	85
+	"Down"
+	70~85
+	4 >>	80
+	"Down"
+	70~80
+	5 >>	75
+	"Up"
+	75~80
+	6 >>	77
+	맞았습니다!! 
+	다시하시겠습니까?(y/n) >>	y 
+	숫자가 정해졌습니다. 맞춰보세요!
+	0~99
+	1 >>	30
+	"Up"
+	30~99
+	2 >>	45
+	맞았습니다!!
+	다시하시겠습니까?(y/n) >> 	n
+	게임 종료!	
+ */
 public class Test45 {
 	public static void main(String[] args) {
 
-		/*	
-			문제1. Up & Down 게임 
-				0 ~ 99 사이의 임의의 수(랜던값)를 받아 숨기고, 그 수를 맞추는 게임. 
-				임의의 수보다 낮게 입력하면 "Up" 출력, 
-				높게 입력하면 "Down" 출력하면서 범위를 좁혀가며 수를 맞춘다. 
-				게임을 반복하기 위해 y/n으로 묻고, n인 경우 프로그램 종료 
+		Scanner sc = new Scanner(System.in);
+		boolean play = true;
+		while(play) {
+			// 임의의 숫자 생성하고 
+			int num = (int)(Math.random() * 100); // 0 ~ 99사이 난수 
+			int guess = -1; // 사용자가 추측하는 숫자 입력받을 변수 
+			int low = 0; 	// 낮은 범위값 변수 
+			int high = 99;  // 높은 범위값 변수 
+			int count = 1;  // 추측 시도하는 횟수용 변수 
+			
+			// 맞추기 게임 진행 
+			System.out.println("숫자가 정해졌습니다. 맞춰보세요!");
+			while(true) {
+				// 맞췄으면 다시 할것인지 물어보기 
+				//-> y = 다시 반복, n = while문 종료 play = false처리 
+				if(guess == num) {
+					System.out.println("맞췄습니다!!");
+					System.out.println("다시하시겠습니까?(y/n) >>");
+					String answer = sc.nextLine(); 
+					if(answer.equalsIgnoreCase("n")) {
+						System.out.println("게임 종료!!!");
+						play = false;  // 겉에 있는 전체 게임 회전 종료
+						break; // 가까운 while true (범위추측while문 종료) 
+					}else if(answer.equalsIgnoreCase("y")) {
+						play = true; 
+						break; // 게임 다시시작하더라고, 추측하던 반복은 종료 
+					}else {
+						System.out.println("잘못입력하셨습니다.");
+						continue; 
+					}
+				}
 				
-			콘솔예) 
-			숫자가 정해졌습니다. 맞춰보세요! 
-			0~99
-			1 >>	55 (사용자가 입력) 
-			"Up"
-			55~99
-			2 >>	70
-			"Up"
-			70~99
-			3 >>	85
-			"Down"
-			70~85
-			4 >>	80
-			"Down"
-			70~80
-			5 >>	75
-			"Up"
-			75~80
-			6 >>	77
-			맞았습니다!! 
-			다시하시겠습니까?(y/n) >>	y 
-			숫자가 정해졌습니다. 맞춰보세요!
-			0~99
-			1 >>	30
-			"Up"
-			30~99
-			2 >>	45
-			맞았습니다!!
-			다시하시겠습니까?(y/n) >> 	n
-			게임 종료!	
-		*/
+				System.out.println(low + "~" + high); // 범위 출력 
+				System.out.println(count + " >> ");
+				guess = Integer.parseInt(sc.nextLine());
+				count++; 
+				
+				if(guess < num) {
+					System.out.println("Up");
+					low = guess;  // 낮은 범위가 변경 
+				}
+				if(guess > num) {
+					System.out.println("Down");
+					high = guess; // 높은 범위가 변경
+				}
+			}
+			
+			
+		}
 		
 		
 		
--- teacher/day04/Test46.java
+++ teacher/day04/Test46.java
@@ -1,5 +1,7 @@
 package day04;
 
+import java.util.Scanner;
+
 public class Test46 {
 	public static void main(String[] args) {
 
@@ -11,18 +13,50 @@ public class Test46 {
 			1	2	3	4
 			1	2
 		*/
+		int [][] arr = {
+				{1}, 
+				{1,2,3},
+				{1},
+				{1,2,3,4},
+				{1,2}}; 
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "  ");
+			}
+			System.out.println();
+		}
 		
 		//문제2. Scanner로 소문자 알파벳 하나 입력받고, 아래와 같이 출력하는 프로그램을 작성하세요. 
-		/*   String.charAt(); --> char --> ascii 숫자
+		/*   String.charAt(idx); --> char --> ascii 숫자
 			소문자 알파벳하나 입력 >> 	e (사용자가 입력) 
-			abcde
-			abcd
-			abc
-			ab
-			a
-		*/
+			abcde		97 98 99 100 101
+			abcd		97 98 99 100
+			abc			97 98 99
+			ab			97 98
+			a 			97
+		
+		System.out.println("소문자 알파벳하나 입력 >> ");
+		Scanner sc = new Scanner(System.in);
+		String alpha = sc.nextLine(); 
+		char ch = alpha.charAt(0);
+		for(int i = ch; i >= 97; i--) {
+			for(int j = 97; j <= i; j++) {
+				System.out.print((char)j);
+			}
+			System.out.println();
+		}
 		
 		//문제3. 양의 정수 10개를 입력받아 배열에 저장하고, 3의 배수만 출력하는 프로그램을 작성하세요. 
+		Scanner sc = new Scanner(System.in);
+		int [] n = new int[10];
+		for(int i = 0; i < n.length; i++) {
+			n[i] = Integer.parseInt(sc.nextLine());
+		}
+		for(int i = 0; i < n.length; i++) {
+			if(n[i] % 3 == 0) {
+				System.out.println(n[i]);
+			}
+		}*/
 		
 		//문제4. 4x4의 2차원 배열(총16개방)을 만들고, 1 ~ 10까지 범위의 정수를 10개만 
 		//		랜덤하게 생성하여 임의의 위치에 삽입하세요. 
@@ -35,6 +69,37 @@ public class Test46 {
 				0	6	4	5
 				0	8	0	0
 		*/
+		System.out.println("----------------------------------");
+		int [][] board = new int[4][4]; 
+		int [] randNums = new int[10]; // 임의의 정수 10개 받아놓을 배열 
+		// board를 0으로 초기화 
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				board[i][j] = 0; 
+			}
+		}
+		// 10개 랜덤 받기 
+		for(int i = 0; i < randNums.length; i++) {
+			randNums[i] = (int)(Math.random() * 10) + 1; 
+		}
+		// 임의의 방에 랜덤받아놓은수 넣기. 방 중복되면 안됨!!
+		for(int i = 0; i < randNums.length; i++) {
+			int x = (int)(Math.random() * 4);
+			int y = (int)(Math.random() * 4);
+			if(board[x][y] != 0) { // 이미 한번 뽑혔던 방 -> for문 횟차 다시 
+				i--; 
+			}else { // 방에 0이 들어있으면 중복X 잘뽑은 랜덤방이므로 
+				board[x][y] = randNums[i]; // 랜덤값으로 방에 넣기 
+			}
+			//System.out.println(i);
+		}
+		// 출력
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j] + "  ");
+			}
+			System.out.println();
+		}
 		
 		
 		
