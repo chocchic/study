7fcabcc0642b0d58d2d42720da6ed767cdd155d9
chocchic <60125582+chocchic@users.noreply.github.com>
2022-03-21 10:02:37 +0900

    Add files via upload

---
+++ spring0203/src/main/java/com/test/model/TestDTO.java
@@ -0,0 +1,12 @@
+package com.test.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+public class TestDTO {
+	private String id;
+	private String pw;
+	private int age;
+	private long longVal; 
+}
