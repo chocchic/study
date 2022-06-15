27c3d2f6f0db29b0ff094da2b0584212dbbacb6b
chocchic <dntksemfdj473@gmail.com>
2022-06-15 12:22:52 +0900

    0615 3

---
+++ react_native/MobileServer/src/main/java/com/choc/MobileServerApplication.java
@@ -0,0 +1,13 @@
+package com.choc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileServerApplication.class, args);
+	}
+
+}
