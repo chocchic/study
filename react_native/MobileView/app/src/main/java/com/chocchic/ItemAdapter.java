7d11ae76c2110d3b2bc8719a866f14dcdd5bfa9f
chocchic <dntksemfdj473@gmail.com>
2022-06-20 16:30:42 +0900

    0620 6

--- react_native/MobileView/app/src/main/java/com/chocchic/ItemAdapter.java
+++ react_native/MobileView/app/src/main/java/com/chocchic/ItemAdapter.java
@@ -1,13 +1,23 @@
 package com.chocchic;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
-
+// 아이폰은 implements, 안드로이드는 extends
 public class ItemAdapter extends BaseAdapter {
     // AdapterView 출력을 위한 속성
     // ** 셀을 만들기 위해서 필요한 정보 ** //
@@ -46,10 +56,66 @@ public class ItemAdapter extends BaseAdapter {
         return data.get(i).getItemid();
     }
 
+    // 실제 셀의 모양을 결정하는 메서드
+    // i는 각 셀의 인덱스, View는 화면에 보여지는 뷰로 처음에는 null이 넘어오고 두 번째부터 이전에 출력한 뷰가 넘어옴.
+    // viewGroup은 이 항목을 사용하는 AdapterView
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
-    }
-    // 아이폰은 implements, 안드로이드는 extends
+        // 매개변수로 넘어온 변수를 다른 변수나 final 변수에 저장하는 경우
+        final int pos = i;
 
+        // 이전에 출력된 뷰가 없다면 뷰를 생성
+        if(view == null){
+            view = inflater.inflate(layout, viewGroup, false);
+        }
+
+        //  i 번째 데이터 찾아오기
+        Item item = data.get(i);
+
+        // 데이터 출력
+        TextView txtName = (TextView)view.findViewById(R.id.itemname);
+        txtName.setText(item.getItemname());
+
+        TextView txtPrice = (TextView)view.findViewById(R.id.price);
+        txtPrice.setText(item.getPrice()+"원");
+
+        ImageView imageView = (ImageView)view.findViewById(R.id.itemimage);
+
+        Handler handler = new Handler(Looper.getMainLooper()) {
+           public void handleMessage(Message msg){
+                // 스레드가 전달한 데이터 읽기
+               Bitmap bitmap = (Bitmap)msg.obj;
+               // 이미지 뷰에 이미지 출력
+               imageView.setImageBitmap(bitmap);
+           }
+        };
+
+        class ThreadEx extends Thread {
+            public void run(){
+                // 이미지를 다운로드
+                try{
+                    // 이미지를 웹 서버의 이미지 다운로드 경로에서 스트림으로 연결
+                    // 문자열을 제외한 모든 데이터의 다운로드
+                    InputStream inputStream = new URL("http://192.168.0.9:80/member/download?path="
+                            + item.getPictureurl()).openStream();
+                    // 이미지의 경우는 다운로드받은 내용을 Bitmap으로 변환
+                    // 이미지가 아닌 경우라면 Stream의 내용을 읽어서 파일로 저장해야 함
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                    // 핸들러에게 데이터를 전달하기 위해서 Message객체를 생성
+                    Message msg = new Message();
+                    msg.obj = bitmap;
+                    
+                    // 핸들러에게 순서대로 작업을 수행하도록 요청
+                    handler.sendMessage(msg);
+                }catch (Exception e){
+                    Log.e("다운로드 예외",e.getLocalizedMessage());
+                }
+            }
+        }
+
+        new ThreadEx().start();
+
+        return view;
+    }
 }
